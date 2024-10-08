5d2055d5833aa440dee1d56450d953d4408b252b
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-08 13:46:38 -0300

    tipando as rotas

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,31 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-const { Navigator, Screen } = createBottomTabNavigator();
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from "@react-navigation/bottom-tabs";
 
 import { Home } from "@screens/Home";
 import { History } from "@screens/History";
 import { Profile } from "@screens/Profile";
 import { Exercise } from "@screens/Exercise";
 
+type AppRoutes = {
+  home: undefined;
+  history: undefined;
+  profile: undefined;
+  exercise: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Screen name="home" component={Home} />
 
       <Screen name="history" component={History} />
