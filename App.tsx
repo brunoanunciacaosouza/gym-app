d44a89ca46928f0db790c758c4b7c54df6438f7d
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-08 13:18:34 -0300

    criando contexto de navegacao

--- App.tsx
+++ App.tsx
@@ -9,8 +9,7 @@ import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 
 import { Loading } from "@components/Loading";
-import { SignIn } from "@screens/SignIn";
-import { SignUp } from "@screens/SignUp";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +25,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   );
 }
