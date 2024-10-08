33fad0d7dc360638bfe8d8582b087c4e114c831f
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-08 14:51:30 -0300

    Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,16 @@
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
-    <HStack bg="#202024" pt="$16" pb="$5" px="$8" alignItems="center">
+    <HStack bg="#202024" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: "https://avatars.githubusercontent.com/u/85529074?v=4" }}
+        alt="Imagem do usuário"
+        w="$16"
+        h="$16"
+      />
+
       <VStack>
         <Text color="#E1E1E6" fontSize="$sm">
           Olá,{" "}
