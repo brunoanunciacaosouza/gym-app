5752d1017b825ce82ade05575bdf30dd38dbdbb8
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-10 15:46:13 -0300

    estilizando inputs invalidos

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -24,15 +24,20 @@ export function Input({
   return (
     <FormControl isInvalid={inValid} w="$full" mb="$2">
       <GlueStackInput
+        isInvalid={isInvalid}
         h="$12"
         borderWidth="$0"
         borderRadius="$md"
         $focus={{
           borderWidth: 1,
-          borderColor: "$green500",
+          borderColor: inValid ? "$red500" : "$green500",
         }}
         isReadOnly={isReadOnly}
         opacity={isReadOnly ? 0.5 : 1}
+        $invalid={{
+          borderWidth: 1,
+          borderColor: "$red500",
+        }}
       >
         <InputField
           bg="#121214"
