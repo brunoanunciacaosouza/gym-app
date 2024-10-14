deab37c9ebc62964b33aebab15ba5d09de3368ca
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-14 13:58:40 -0300

    custom Hook useAuth

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,17 +1,14 @@
-import { useContext } from 'react';
-
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 import { Box } from '@gluestack-ui/themed';
 
-import { AuthContext } from '@contexts/AuthContext';
-
 import { AuthRoutes } from './auth.routes';
+import { useAuth } from '@hooks/useAuth';
 
 export function Routes() {
-  const {} = useContext(AuthContext);
-  const theme = DefaultTheme;
+  const { user } = useAuth();
 
+  const theme = DefaultTheme;
   theme.colors.background = '#121214';
 
   return (
