4d4180b4985dafedc0cb69fd0e807c7e2b937a07
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-15 11:31:39 -0300

    exibindo nome do usuario

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -1,12 +1,14 @@
-import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
-import { UserPhoto } from "./UserPhoto";
-import { LogOut } from "lucide-react-native";
+import { Heading, HStack, Text, VStack, Icon } from '@gluestack-ui/themed';
+import { UserPhoto } from './UserPhoto';
+import { LogOut } from 'lucide-react-native';
+import { useAuth } from '@hooks/useAuth';
 
 export function HomeHeader() {
+  const { user } = useAuth();
   return (
     <HStack bg="#202024" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
       <UserPhoto
-        source={{ uri: "https://avatars.githubusercontent.com/u/85529074?v=4" }}
+        source={{ uri: 'https://avatars.githubusercontent.com/u/85529074?v=4' }}
         alt="Imagem do usuário"
         w="$16"
         h="$16"
@@ -14,11 +16,11 @@ export function HomeHeader() {
 
       <VStack flex={1}>
         <Text color="#E1E1E6" fontSize="$sm">
-          Olá,{" "}
+          Olá,{' '}
         </Text>
 
         <Heading color="#E1E1E6" fontSize="$md">
-          Bruno Souza
+          {user?.name}
         </Heading>
       </VStack>
 
