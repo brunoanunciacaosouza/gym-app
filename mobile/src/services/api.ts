f47a4779cb6e17c100bbe478b83e13cbed1095f2
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-21 08:49:52 -0300

    verificando se o token é valido

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,19 +1,41 @@
+import axios, { AxiosInstance } from 'axios';
 import { AppError } from '@utils/AppError';
-import axios from 'axios';
+
+type SignOut = () => void;
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
 
 export const api = axios.create({
   baseURL: 'http://10.0.0.149:3333/',
-});
+}) as APIInstanceProps;
 
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
-    } else {
-      return Promise.reject(new AppError(error));
-    }
-  },
-);
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTOkenManager = api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (requestError) => {
+      if (requestError?.response?.status === 401) {
+        if (
+          requestError.response.data?.message === 'token.expirado' ||
+          requestError.response.data?.message === 'token.invalid'
+        ) {
+        }
+
+        signOut();
+      }
+
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message));
+      } else {
+        return Promise.reject(new AppError(requestError));
+      }
+    },
+  );
+
+  return () => {
+    api.interceptors.response.eject(interceptTOkenManager);
+  };
+};
